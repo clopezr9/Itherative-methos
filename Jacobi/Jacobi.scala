aac20ae01ddaea0768213c37d75f5bbe816471a9
Shiroke-013 <Shiroke4321@gmail.com>
2021-05-31 14:58:05 -0500

    Jacbi

--- Jacobi/Jacobi.scala
+++ Jacobi/Jacobi.scala
@@ -1,31 +1,21 @@
 import scala.io.Source
 import javax.print.DocFlavor.INPUT_STREAM
 object Jacobi {
-	def main(args: Array[String]) {
+	def main(args: Array[String]): Unit = {
       
-        var C = readCSV()
-        var l = C.length
-        var A = Array.ofDim[Double](l, l) 
-        var B = Array.ofDim[Double](l, 1) 
+        var ab = readCSV()
+        var (a,b,l) = ab
         var N = 500
-        var ig = Array.fill(l)(0.0)
         var tolerancia: Double = 0.00001
+        jacobi(a,b,l,N)
 
-        for(i <- 0 to (l-1)){
-           for (j <- 0 to (l-1)){
-              A(i)(j) = C(i)(j)
-           }
-         }
+    }
 
-        println("A.length: ", A(0).length)
+   def jacobi(A: Array[Array[Double]], B:Array[Array[Double]], l:Int, N:Int) {
 
-        for(i <- 0 to l-1){
-            B(i)(0) = C(i)(3)
-            println("Position: ",i,0)
-            println("B: ",B(i)(0))
-         }
+      var ig = Array.fill(l)(0.0)
 
-         var D = Array.fill(l)(0.0)
+      var D = Array.fill(l)(0.0)
          for(i <- 0 to l-1){
             for(j <- 0 to l-1){
                if(i == j){
@@ -36,32 +26,44 @@ object Jacobi {
             }
          }
 
-         var R = Array.ofDim[Double](l, l) 
-         for(i <- 0 to l-1){
-            for(j <- 0 to l-1){
-               if(i != j){
-                  R(i)(j) = A(i)(j)
-                  //println("Position of R: ",i,j)
-                  //println("R: " , R(i)(j))
-               }
+      var R = Array.ofDim[Double](l, l) 
+      for(i <- 0 to l-1){
+         for(j <- 0 to l-1){
+            if(i != j){
+               R(i)(j) = A(i)(j)
+               //println("Position of R: ",i,j)
+               //println("R: " , R(i)(j))
             }
          }
-         var iteracion = 0
-         while(iteracion < N){
-            ig = division(substraction(B, dotProduct(R, ig, l),l), D, l)
-            iteracion = iteracion + 1
-         }
-         for(i<-0 to l-1){
-            println(ig(i))
-         }
-    }
+      }
 
-   def readCSV() : Array[Array[Double]] = {
-      io.Source.fromFile("test.csv")
-      .getLines()
-      .map(_.split(",").map(_.trim.toDouble))
-      .toArray
+      var iteracion = 0
+      while(iteracion < N){
+         ig = division(substraction(B, dotProduct(R, ig, l),l), D, l)
+         iteracion = iteracion + 1
+      }
+      for(i<-0 to l-1){
+         println(ig(i))
+      }
+   }
 
+   def readCSV() : (Array[Array[Double]], Array[Array[Double]], Int) = {
+      var C = io.Source.fromFile("test.csv").getLines().map(_.split(",").map(_.trim.toDouble)).toArray
+      var l = C.length
+      var A = Array.ofDim[Double](l, l) 
+      for(i <- 0 to (l-1)){
+         for (j <- 0 to (l-1)){
+            A(i)(j) = C(i)(j)
+         }
+      }
+
+      var B = Array.ofDim[Double](l, l) 
+      for(i <- 0 to l-1){
+            B(i)(0) = C(i)(3)
+            //println("Position: ",i,0)
+            //println("B: ",B(i)(0))
+         }
+      return (A,B,l)
    }
 
    def dotProduct(R: Array[Array[Double]], b:Array[Double], l:Int): Array[Double] = {
