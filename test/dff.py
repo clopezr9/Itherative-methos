a9a25458fdd644157f9e576d33a392b9caccbd88
SamtiagoSR <samtiago.santa.ramirez@gmail.com>
2021-05-31 16:35:36 -0500

    adding test generator

---
+++ test/dff.py
@@ -0,0 +1,44 @@
+import numpy as np 
+
+def isDDM(m, n) :
+ 
+    # for each row
+    for i in range(0, n) :        
+     
+        # for each column, finding
+        # sum of each row.
+        sum = 0
+        for j in range(0, n) :
+            sum = sum + abs(m[i][j])    
+ 
+        # removing the
+        # diagonal element.
+        sum = sum - abs(m[i][i])
+ 
+        # checking if diagonal
+        # element is less than
+        # sum of non-diagonal
+        # element.
+        if (abs(m[i][i]) < sum) :
+            m[i][i] += sum 
+            return False
+    return True
+
+# def randomGenerator() :
+#     matrix = np.random.rand(501,501)
+#     return matrix
+
+# Driver Code
+n = 2000
+m = np.random.rand(2000,2001)
+while True :
+    if((isDDM(m, n))) :
+        print (m)
+        np.savetxt("2000-0.csv", m, 
+              delimiter = ",")
+        break
+    else :
+        continue
+
+
+ 
