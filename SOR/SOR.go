f830495e702a4c2e7ad27c81f370caa828b2a534
SamtiagoSR <samtiago.santa.ramirez@gmail.com>
2021-06-02 10:54:41 -0500

    Adding go

--- SOR/SOR.go
+++ SOR/SOR.go
@@ -15,7 +15,7 @@ func main() {
 	args := os.Args
 	for i := 1; i < len(args); i++ {
 		file_name := args[i]
-		fmt.Println(file_name, ":")
+		fmt.Println(args, ":")
 		sor_solver(readCSVFile(file_name)) //n = system dimensions, A = augmented matrix
 	}
 }
@@ -24,6 +24,7 @@ func readCSVFile(file_name string) ([]Array, []float64) {
 	if err != nil {
 		fmt.Println(err)
 	}
+	fmt.Println("Successfully Opened CSV file")
 	defer csvFile.Close()
 
 	csvLines, err := csv.NewReader(csvFile).ReadAll()
@@ -53,26 +54,56 @@ func readCSVFile(file_name string) ([]Array, []float64) {
 	return matrix, b
 }
 
+func euclidian_distance(A []float64, b []float64) float64 {
+	var distance = 0.0
+	for fila := 0; fila < len(A); fila++ {
+		distance = distance + math.Pow(A[fila]-b[fila], 2)
+	}
+
+	distance = math.Sqrt(distance)
+	return distance
+}
+func dot(A []Array, b []float64, l int) []float64 {
+
+	result := make([]float64, l)
+	for i := 0; i < l; i++ {
+		var aux = 0.0
+		for j := 0; j < l; j++ {
+			aux = aux + b[j]*A[i][j]
+		}
+		result[i] = aux
+	}
+	return result
+}
+func substraction(a []float64, b []float64, l int) []float64 {
+
+	result := make([]float64, l)
+	for i := 0; i < l; i++ {
+		result[i] = a[i] - b[i]
+	}
+	return result
+}
+
 func sor_solver(A []Array, b []float64) {
 
 	//Create where the solution will be store
 	var long int = len(b)
 	var X0 = make([]float64, long)
 
-	var tolerancia float64 = 0.0000001
+	var tolerancia float64 = 0.000001 //1.0071743059050822
 	var iteracionMax int = 100
 
 	var n int = len(A)
 	var m int = len(A)
 	var omega float64 = 1.5
 	var X = X0
-	var diferencia = make([]float64, long)
+	var tol float64 = euclidian_distance(dot(A, X, n), b)
 
-	var errado = tolerancia * 2
 	var iteracion int = 0
 	start := time.Now()
 
-	for !(errado <= tolerancia || iteracion > iteracionMax) {
+	for tol > tolerancia && iteracion < iteracionMax {
+
 		for fila := 0; fila < n; fila++ {
 			var suma float64 = 0
 			for columna := 0; columna < m; columna++ {
@@ -81,26 +112,16 @@ func sor_solver(A []Array, b []float64) {
 				}
 			}
 			var nuevo float64 = (1.0-omega)*X[fila] + (omega/A[fila][fila])*(b[fila]-suma)
-			diferencia[fila] = math.Abs(nuevo - X[fila])
 
 			X[fila] = nuevo
 
 		}
-		var max float64
-		for i := 0; i < len(diferencia)-1; i++ {
 
-			if max < diferencia[i] {
-
-				max = diferencia[i]
-			}
-		}
-		errado = max
+		tol = euclidian_distance(dot(A, X, n), b)
 		iteracion = iteracion + 1
 	}
-	// fmt.Println(X)
-	// fmt.Println("")
+	fmt.Println("Respuesta X: ", X)
+	fmt.Println("")
 	elapsed := time.Since(start)
-	fmt.Println(elapsed.Nanoseconds())
-	// elapsed := time.Since(start)
-	// fmt.Println("EXECUTION TIME: ", elapsed.Nanoseconds(), " nanoseconds")
+	fmt.Println("EXECUTION TIME: ", elapsed.Nanoseconds(), " nanoseconds")
 }
